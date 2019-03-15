e69f4943c855f4ecee553a5611a6f5ec6026b017
will1001 <wilirahmatm@gmail.com>
2019-03-16 07:53:12 +0800

    coba sublime merge

--- client/src/App.js
+++ client/src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <h1>test commit bos</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
